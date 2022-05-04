2aa4959baffec3a3ff9db697bea716ba8edeea64
Pablo Giralt Calleja <pablo@MacBook-Pro-de-Pablo-2.local>
2022-05-05 00:04:01 +0200

    remove tailwind css

--- .config/postcss.config.js
+++ .config/postcss.config.js
@@ -3,8 +3,6 @@ const path = require('path')
 module.exports = {
   plugins: [
     require('postcss-import'),
-    require('tailwindcss/nesting'),
-    require('tailwindcss')(path.resolve(__dirname, '../src/tailwind.config.js')),
     require('autoprefixer')
   ]
 }
